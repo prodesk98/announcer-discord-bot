277e0f455a26450e7cf51a750f65d88654cdd448
prodesk98 <133387879+prodesk98@users.noreply.github.com>
2024-05-01 17:15:41 -0300

    lib play audio

---
+++ utils/play_audio.py
@@ -0,0 +1,26 @@
+import asyncio
+from os import PathLike
+
+from discord import Interaction, VoiceState, VoiceClient, utils as discord_utils, FFmpegOpusAudio
+
+
+async def PlayAudio(interaction: Interaction, audio_path: str | PathLike) -> None:
+    if interaction.user.voice is None:
+        await interaction.edit_original_response(content="You must be in a voice channel")
+        return
+
+    voice: VoiceState | VoiceClient = discord_utils.get(interaction.client.voice_clients, guild=interaction.user.guild)  # type: ignore
+    if voice is None:
+        voice = await interaction.user.voice.channel.connect()
+    elif voice.channel != interaction.user.voice.channel:
+        await voice.move_to(interaction.user.voice.channel)
+        await asyncio.sleep(3)
+
+    source = FFmpegOpusAudio(
+        executable="ffmpeg",
+        source=audio_path,
+        **{'options': '-vn'}
+    )
+    if voice.is_playing():
+        voice.stop()
+    voice.play(source)
