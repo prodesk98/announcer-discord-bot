6e19d04fed7aa3cbe10386dc3491b55d6ee0424d
prodesk98 <133387879+prodesk98@users.noreply.github.com>
2024-05-01 15:29:28 -0300

    removed: voice id env

---
+++ README.md
@@ -0,0 +1,21 @@
+# Installation
+
+Copy the .env.example file to .env and the config/.voices.json to config/voices.json
+
+
+#### Copy Env File
+
+``
+copy env.example .env
+``
+
+
+#### Copy Voices Config File
+
+``
+copy config/.voices.json config/voices.json
+``
+
+#### Get voices
+
+[ElevenLabs](https://elevenlabs.io/app/voice-lab)
--- config/environment.py
+++ config/environment.py
@@ -8,15 +8,12 @@ load_dotenv()
 class Environment(BaseModel):
     DISCORD_TOKEN: str = getenv("DISCORD_TOKEN")
     ELEVENLABS_TOKEN: str = getenv("ELEVENS_TOKEN")
-    ELEVENLABS_VOICE_ID: str = getenv("VOICE_ID")
 
     # valid
     if not DISCORD_TOKEN:
         raise ValueError("DISCORD_TOKEN is not set")
     if not ELEVENLABS_TOKEN:
         raise ValueError("ELEVENLABS_TOKEN is not set")
-    if not ELEVENLABS_VOICE_ID:
-        raise ValueError("ELEVENLABS_VOICE_ID is not set")
 
 
 env = Environment()
